434439b7fff2eb947d8c236a3e990a9c43d86af9
felixNyalenda <felixasejunior@gmail.com>
2023-03-29 01:13:37 +0300

    challenge 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", () => {
+   //    console.log("%c HI", "color: firebrick");
+
+   // Challenge 1
+   const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+   const imgContainer = document.querySelector("#dog-image-container");
+   fetch(imgUrl)
+      .then((res) => res.json())
+      .then(({ message }) => {
+         message.forEach((img) => {
+            const imgTag = document.createElement("img");
+            imgTag.src = img;
+            imgTag.style.width = "500px";
+            imgTag.style.height = "500px";
+            imgContainer.append(imgTag);
+         });
+      });
+});
