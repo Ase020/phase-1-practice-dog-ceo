38a50fd580b81e952f37519b6af22a845e66a956
felixNyalenda <felixasejunior@gmail.com>
2023-03-29 09:17:30 +0300

    challenge 2

--- src/index.js
+++ src/index.js
@@ -15,4 +15,19 @@ document.addEventListener("DOMContentLoaded", () => {
             imgContainer.append(imgTag);
          });
       });
+
+   //   Challenge 2
+   const breedUrl = "https://dog.ceo/api/breeds/list/all";
+   const ulDogBreeds = document.getElementById("dog-breeds");
+   fetch(breedUrl)
+      .then((res) => res.json())
+      .then((breeds) => {
+         const dogBreedsArray = Object.keys(breeds.message);
+         dogBreedsArray.forEach((breeds) => {
+            const li = document.createElement("li");
+            li.textContent = breeds;
+            li.style.cursor = "pointer";
+            ulDogBreeds.append(li);
+         });
+      });
 });
